49172d492ff03738c0d484ddf0fb41f694fb5d70
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-01-04 15:57:42 +0100

    Tipos inferidos 1

---
+++ src/03-typing.ts
@@ -0,0 +1,11 @@
+let myProductName = 'Product 1';
+let myProductPrice = 123;
+
+myProductName = 'Product 2';
+myProductName.toLocaleUpperCase();
+
+myProductPrice.toFixed();
+
+const myProductStock = 1000;
+const myProductName2 = 'Product 2';
+
